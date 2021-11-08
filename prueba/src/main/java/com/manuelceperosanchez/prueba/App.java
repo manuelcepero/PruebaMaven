0f695a98df5e8e31933091d302fd6cdaf0564f75
Manuel Cepero Sánchez <manuel.cepero@hotmail.com>
2021-11-08 14:37:44 +0100

    Personas y coche añadidos

--- prueba/src/main/java/com/manuelceperosanchez/prueba/App.java
+++ prueba/src/main/java/com/manuelceperosanchez/prueba/App.java
@@ -15,5 +15,17 @@ public class App
     public static void main( String[] args )
     {
     	System.out.println("Hello world!");
+    	
+    	Persona persona1 = new Persona("Paco");
+        Persona persona2 = new Persona("Carlos");
+        
+        List<Persona> personas = new ArrayList();
+        personas.add(persona1);
+        personas.add(persona2);
+        
+        Coche coche1 = new Coche(personas);
+        
+        System.out.println(persona1.nombre);
+        System.out.println(persona2.nombre);
     }
 }
